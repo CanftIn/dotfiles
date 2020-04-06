3d9ba70e4e1708a61605f7beea67cd7ea93c4258
CanftIn <wwc7033@gmail.com>
2020-04-06 20:55:40 +0800

    Create profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,13 @@
+{:user
+  {
+  :java-cmd "C:\\Program Files\\Java\\jdk1.8.0_241\\bin\\java.exe"
+  :plugins [
+
+    ]
+   }
+
+  :java-13-source
+  {:extra-deps
+   {java-sources {:local/root "C:/Program Files/Java/jdk-13.0.2/lib/src.zip"}}}
+ }
+
